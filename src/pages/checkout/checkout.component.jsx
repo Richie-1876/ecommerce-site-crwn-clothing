bf323bc38f7e380b297cda68daf199bfdca2031d
Richard Wright <richardwright@Richards-MacBook-Pro-1.local>
2020-09-04 15:48:08 +0100

    Updated strip integration and updated favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,7 @@ import {
 } from "../../redux/cart/cart.selectors";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import "./checkout.styles.scss";
 
@@ -35,6 +36,14 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      Card Num: 4242 4242 4242 4242 <br />
+      Exp: Any date in the future <br />
+      cvv: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
