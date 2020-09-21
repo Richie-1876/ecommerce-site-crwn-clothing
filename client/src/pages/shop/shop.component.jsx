5d0a3ccba9a3089920ebd3579c7610d59a47bf54
Richard Wright <richardwright@Richards-MacBook-Pro-1.local>
2020-09-21 17:13:21 +0100

    Optimized app performance by using react lazy loading and suspense to chunk up code based on routes

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,10 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
+import Spinner from "../../components/spinner/spinner.component";
 
-import CollectionPageContainer from "../collection/collection.container";
-import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
+/*********************************************************
+              ^^^^^^^^^^^IMPORTS^^^^^^^^^^^^^^^
+**********************************************************/
+
+const CollectionPageContainer = lazy(() =>
+  import("../collection/collection.container")
+);
+const CollectionsOverviewContainer = lazy(() =>
+  import("../../components/collections-overview/collections-overview.container")
+);
+
+/*********************************************************
+              ^^^^^^^^^^^LAZY IMPORTS^^^^^^^^^^^^^^^
+**********************************************************/
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
   useEffect(() => {
@@ -12,15 +25,17 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
   }, [fetchCollectionsStart]);
   return (
     <div className="shop-page">
-      <Route
-        exact
-        path={`${match.path}`}
-        component={CollectionsOverviewContainer}
-      />
-      <Route
-        path={`${match.path}/:collectionId`}
-        component={CollectionPageContainer}
-      />
+      <Suspense fallback={<Spinner />}>
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
+      </Suspense>
     </div>
   );
 };
